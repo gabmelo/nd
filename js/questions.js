23296fbb2d1452d3e12a02cfda53ca08dc6f45e6
gabmelo <70192926+gabmelo@users.noreply.github.com>
2021-12-06 01:44:16 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual é a minha formação?",
+    answer: "Física Licenciatura",
+    options: [
+      "Educação Física",
+      "Matemática Licenciatura",
+      "Química Licenciatura",
+      "Física Licenciatura"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quantos gatos eu tenho?",
+    answer: "11",
+    options: [
+      "2",
+      "4",
+      "7",
+      "11"
+    ]
+  },
+{
+    numb: 3,
+    question: "O que penso sobre as TDIC?",
+    answer: "É algo mais amplo",
+    options: [
+      "São maravilhosas",
+      "São prejudiciais",
+      "Possuem aspectos positivos e negativos",
+      "É algo mais amplo"
+    ]
+  },
+
+    {
+    numb: 4,
+    question: "Como reagi ao perceber que TDIC vão além de artefatos tecnológicos?",
+    answer: "Surpreso",
+    options: [
+      "Surpreso",
+      "Indiferente",
+      "Assustado",
+      "contundido"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Como me sinto escrevendo uma Narrativa Digital?",
+    answer: "Instigado",
+    options: [
+      "Cansado",
+      "Desafiado",
+      "Instigado",
+      "Surtado"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
